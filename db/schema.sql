8eb7f20b81247b4cf7dcb211291f69095bcb6cca
Mark Powalisz <markpowalisz@Fenix-Macbook-Pro.local>
2018-01-22 19:18:19 -0500

    Enabling connection to JawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE dogs_db;
 USE dogs_db;
 
-CREATE TABLE dogs
-(
-	id int NOT NULL AUTO_INCREMENT,
-    dog_name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
-);
+CREATE TABLE `dogs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `spayed` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+)
