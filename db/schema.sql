af96e885b6a2d8fe371550467f9b46a5f6f50668
mpowalisz <markpowalisz@gmail.com>
2018-08-24 14:56:10 -0400

    Updated database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,12 @@
+DROP DATABASE IF EXISTS dogs_db
 CREATE DATABASE dogs_db;
 USE dogs_db;
 
-CREATE TABLE `dogs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `spayed` tinyint(1) NOT NULL DEFAULT '0',
+CREATE TABLE dogs (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  spayed tinyint(1) NOT NULL DEFAULT '0',
   createdAt TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (id)
 )
+
